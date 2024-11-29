b208866407e707b6e8ff0babb623706c4e17326c
Camomilte <camomilte@gmail.com>
2024-11-29 01:07:45 +0100

    styling done

--- script.js
+++ script.js
@@ -1,15 +1,26 @@
-/* To Do:
-    - Must only be text
-    - Max length of ???
-    - Must not render as HTML
-*/
-
+/* 
 const inputField = document.getElementById('inputField');
-const toDoContainer = document.getElementById('toDoContainer');
+const listContainer = document.getElementById('listContainer');
 const addButton = document.getElementById('addButton');
 
+
+
 addButton.addEventListener('click', function() {
-    var toDo = document.createElement('p')
+    if(inputField.value == ""){
+        document.getElementById('error-msg').style.display = "block"
+        return
+    }
+    document.getElementById('error-msg').style.display = "none";
+    let toDo = document.createElement('li');
+    toDo.classList.add('todo-task');
     toDo.innerText = inputField.value;
-    toDoContainer.appendChild(toDo)
-});
+    listContainer.appendChild(toDo);
+    inputField.value = "";
+}); */
+
+const todoForm = document.querySelector('form');
+const inputField = document.getElementById('inputField');
+const addButton = document.getElementById('addButton')
+const todoList = document.getElementById('listContainer');
+
+console.log(todoList)
